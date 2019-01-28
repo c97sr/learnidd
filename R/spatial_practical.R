6d2138813894a79cb091e4426c45005ee9493bfd
Kylie Ainslie <ainslie.kylie@gmail.com>
2019-01-28 13:53:03 +0000

    update spatial practical code
    Include information for help file generation

--- R/spatial_practical.R
+++ R/spatial_practical.R
@@ -17,24 +17,37 @@
 
 # You may need something similar to setwd("/Users/sriley/Dropbox/talks/20120309_MSc_Adv_Spatial/")
 # A function to load up a file and prepare objects to be used for simulation
+
+#' sets up data set for simulation
+#' 
+#' @param df data frame with input data for simulation
+#' @return data frame with additional columns 'seqid' and 'g_i'
 #' @export
 
 adm.load.sheep.prep.sim <- function(
+    df = farmdat
 		#filename="msc_farm_data",
-		max_kernel_dist=15000,
-		simsteps=0:100) {
+		#max_kernel_dist=15000,
+		#simsteps=0:100
+    ) {
 	
 	# Load up the file
 	#dat <- read.csv(filename)
-  dat <- farmdat
+  dat <- df
 	nofarms <- dim(dat)[1]
 	
-	# Add dummy rows for the different elements of the simulation
+	# Add dummy columns for the different elements of the simulation
 	dat <- cbind(seqid=1:nofarms,dat,g_i=rep(-1,nofarms))
 	
 	dat
 	
 }
+
+#' creates distance matrix
+#' 
+#' @param df data frame
+#' @return distance matrix 
+#' @export
 
 adm.setup.aux.mat <- function(df) {
 	nf 	<- dim(df)[1]
@@ -54,8 +67,15 @@ adm.setup.aux.mat <- function(df) {
 	rtn
 }
 
-# Function to see farms randomly near to the coast
-# Returns the value of seeds as UIDs
+
+#' Function to see farms randomly near to the coast
+#' 
+#' @param tab data frame with farm coordinates
+#' @param nseed number of seeds
+#' @param sdsq vector of bounds for easting and northing coordinates
+#' @return the value of seeds as UIDs
+#' @export
+
 adm.seed.sheep <- function(tab,nseed=1,sdsq=c(0,10000000,0,1000000)) {
 	subtab <- tab[	tab$easting > sdsq[1] &
 					tab$easting < sdsq[2] &
@@ -72,20 +92,43 @@ adm.seed.sheep <- function(tab,nseed=1,sdsq=c(0,10000000,0,1000000)) {
 	x
 }
 
+#' Function to see farms randomly near to the coast
+#' 
+#' @param tab data frame with farm coordinates
+#' @param nseed number of seeds
+#' @param sdsq vector of bounds for easting and northing coordinates
+#' @return the value of seeds as UIDs
+#' @export
+
+#' Function to plot farms randomly near to the coast
+#' 
+#' @param tab data frame with farm coordinates
+#' @param vecseeds vector of seeds
+#' @return plot of randomly selected farms near the coast
+#' @export
+
 adm.plot.farm.and.seeds <- function(tab,vecseeds) {
 	subtab <- tab[tab$seqid %in% vecseeds,]
 	plot(tab$easting,tab$northing)
 	points(subtab$easting,subtab$northing,col="red",cex=1,pch=19)
 }
 
-# Assumes the existance of farmdf in an enclosing environment
-# Essentially a void function so returns a 0 for successful execution
+#' Assumes the existance of farmdf in an enclosing environment
+#' 
+#' @param s index of return vector
+#' @param df data frame with farm coordinates
+#' @param newgen indicator variable for whether there is a new generation (0=no, 1=yes)
+#' @return Essentially a void function so returns a 0 for successful execution
+#' @export
+
 adm.apply.seed <- function(s,df,newgen=0) {
 	nf <- dim(df)[1]
 	rtn <- rep(-1,nf)
 	rtn[s] <- newgen
 	rtn
 }
+
+#' @export
 
 # plot((1:100)*1000,sapply((1:100)*1000,adm.offset.kernel),type="l",log="x")
 adm.offset.kernel <- function(d,p=adm.sheep.params()) {
@@ -94,6 +137,7 @@ adm.offset.kernel <- function(d,p=adm.sheep.params()) {
 	rtn
 }
 
+#' @export
 # This function assumes the existance of a farmdf object in the calling scope
 adm.apply.gen.model <- function(p,df,distmatrix=NULL) {
 	nf <- dim(df)[1]
@@ -129,13 +173,22 @@ adm.apply.gen.model <- function(p,df,distmatrix=NULL) {
 	rtn
 }
 
-
+#' Function to plot the generations of the simulation
+#' @param datf data frame with farm coordinates and generation times
+#' @param gens vector of generations to be plotted
+#' @param cols vector of point colors for each generation (should be same length as gens vecor)
+#' @return plot of the modelled disease spread with a color indicating a different generation
+#' @export
 adm.plot.gen <- function(datf,gens=c(0,1,2,3),cols=c("red","blue","green","orange")) {
 	plot(datf[,"easting"],datf[,"northing"])
 	for (i in 1:length(gens)) points(datf[datf$g_i==gens[i],"easting"],datf[datf$g_i==gens[i],"northing"],pch=19,col=cols[i])
 }
 
-# Function to setup a parameter object for the smv simulations
+#' Function to setup a parameter object for the smv simulations
+#' 
+#' @return vector of parameter values
+#' @export
+
 adm.sheep.params <- function() {
 	rtn <- c(	offset=1000,
 				power=2,
